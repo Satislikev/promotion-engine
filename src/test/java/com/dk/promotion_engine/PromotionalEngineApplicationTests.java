837819318ee24de138bb638d643f2eca376f73af
Kamyar Aflaki <kamy.aflaki@gmail.com>
2020-12-10 02:25:49 +0100

    Unit Test update

--- src/test/java/com/dk/promotion_engine/PromotionalEngineApplicationTests.java
+++ src/test/java/com/dk/promotion_engine/PromotionalEngineApplicationTests.java
@@ -6,29 +6,106 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dk.promotion_engine.promotions.BulkPromotion;
 
 class PromotionalEngineApplicationTests {
 
+	private Product a = new Product("A", 50.00);
+	private Product b = new Product("B", 30.00);
+	private Product c = new Product("C", 20.00);
+	private Product d = new Product("D", 15.00);
+
+	private IPromotion bulkPromotion;
+
+
+	private Order order;
+	private List<Product> products;
+
+	@BeforeEach
+	public void before() throws Exception {
+		bulkPromotion = new BulkPromotion();
+
+		products = new ArrayList<>();
+
+	}
+
 	@Test
 	void ValidateProductAndOrder() {
-		Product a = new Product("A",50.00);
-		Product b = new Product("B",30.00);
-		Product c = new Product("C",20.00);
-		Product d = new Product("D",15.00);
-		
-		List<Product> products = new ArrayList<>();
+
 		products.add(a);
 		products.add(b);
 		products.add(c);
 		products.add(d);
-		
-		Order order = new Order(products);
-		
+
+		order = new Order(products);
+
 		assertEquals(a.getId(), "A");
 		assertEquals(a.getPrice(), 50.00);
 		assertNotNull(order.getProducts());
+	}
+
+	@Test
+	void ValidateScenario1() {
+
+		products.add(a);
+
+		products.add(b);
+
+		products.add(c);
+
+		Order order = new Order(products);
+		bulkPromotion.applyRule(order);
+		assertEquals(order.getOrderTotal(), 100.00);
+
+	}
+	
+	@Test
+	void ValidateScenario2() {
+
+		products.add(a);
+		products.add(a);
+		products.add(a);
+		products.add(a);
+		products.add(a);
+
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+
+		products.add(c);
+
+		Order order = new Order(products);
+		bulkPromotion.applyRule(order);
+		assertEquals(order.getOrderTotal(), 370.00);
+
+	}
+	
+	@Test
+	void ValidateScenario3() {
+
+		products.add(a);
+		products.add(a);
+		products.add(a);
+
+
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+
+		products.add(c);
+		products.add(d);
+
+		Order order = new Order(products);
+		bulkPromotion.applyRule(order);
+		assertEquals(order.getOrderTotal(), 280.00);
+
 	}
 
 }
